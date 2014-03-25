de54c0c34af4de65f976be26ff9c482841ee289d
Alex Pivovarov <alex@revivermx.com>
2014-03-24 18:36:53 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,2 @@
+ant debug
+adb install -r bin/App1u-debug.apk
---
+++ src/org/x4444/app1u/MainActivity.java
@@ -0,0 +1,35 @@
+
+package org.x4444.app1u;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void button1Click(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText1 = (EditText)findViewById(R.id.editText1);
+        String msg = editText1.getText().toString();
+
+        intent.putExtra("mymsg", msg);
+        startActivity(intent);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
